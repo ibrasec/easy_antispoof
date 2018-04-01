e6b6d70f9d7acb6e81a6741c1d88441431fca5dd
ibrasec <ibrahim@example.com>
2018-04-01 23:56:36 +0200

    Enhance subnet check

--- easy_antispoof.py
+++ easy_antispoof.py
@@ -29,22 +29,26 @@ interface = raw_input("Enter the FastEthernet/GigabitEthernet interface number (
 how_many = input("Enter how many public subnets are assigned to the customer (ex: 1)\n")
 
 
+def check_subnet(sub):
+        while True:
+                if isvalidip( sub):
+                        if '/' not in sub:
+                                sub = raw_input("[!] Please Re-Enter the subnet followed by subnet mask\n")
+                        else:
+                                return sub
+                                #break
+                else:
+                        sub = raw_input("Please Enter a vaild ip subnet\n")
+                        continue 
+
+
 def get_subnets(i):
         subnet_list = list()
         for iteration in range(i):
-                while True:
-                        public_subnet = raw_input("Enter the public subnet #" + str(iteration+1) + " (ex: 1.1.1.0/30)\n").strip() 
-                        if isvalidip( public_subnet):
-                                print public_subnet
-                                if '/' not in public_subnet:
-                                        print("[!] Please Re-Enter the subnet followed by subnet mask")
-                                else:
-                                        subnet_list.append( public_subnet )
-                                        break
-                        else:
-                                print("Please Enter a vaild ip subnet")
-                                continue
+                public_subnet = raw_input("Enter the public subnet #" + str(iteration+1) + " (ex: 1.1.1.0/30)\n").strip() 
+                subnet_list.append( check_subnet(public_subnet) )
         return subnet_list
+
 
 
 def get_wildcard_mask(subnet):
@@ -53,8 +57,10 @@ def get_wildcard_mask(subnet):
         wildcard_mask = '.'.join([ ma[i] for i in mask.split('.') ])
         return wildcard_mask
 
+
+
 pub = get_subnets(how_many)
-priv = raw_input("Enter the private subnet (ex: 172.16.40.0/30)\n")
+priv = check_subnet( raw_input("Enter the private subnet (ex: 172.16.40.0/30)\n") )
 
 print "\n\nip access-list ex Antispoof_"+cust
 print " permit ip",str(IPNetwork(priv).network),str(IPNetwork(priv).hostmask),str(IPNetwork(priv).network),str(IPNetwork(priv).hostmask)
