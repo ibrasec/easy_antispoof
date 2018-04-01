0222ee06f3ed0daecad03b12109dc6132968fa27
ibrasec <ibrahim@example.com>
2018-04-01 10:39:01 +0200

    Adjuts getwild function to be get_wildcard_mask

--- easy_antispoof.py
+++ easy_antispoof.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python2.7
-
 from netaddr import IPNetwork
 cust = raw_input("Enter Customer Name\n")
 #vlan = raw_input("Enter vlan id\n")
@@ -11,11 +10,11 @@ def get_subnets(i):
         return [ raw_input("Enter the public subnet (ex: 1.1.1.0/30)\n").strip() for i in range(i) ]
 
 
-def getwild(subnet):
+def get_wildcard_mask(subnet):
         mask = str( IPNetwork(subnet).netmask )
         ma={'0':'255','128':'127','192':'63','224':'31','240':'15','248':'7','252':'3','254':'1','255':'0'}
-        wild_mask = '.'.join([ ma[i] for i in mask.split('.') ])
-        return wild_mask
+        wildcard_mask = '.'.join([ ma[i] for i in mask.split('.') ])
+        return wildcard_mask
 
 pub = get_subnets(how_many)
 priv = raw_input("Enter the private subnet (ex: 172.16.40.0/30)\n")
@@ -28,7 +27,7 @@ print " deny ip any 10.0.0.0 0.255.255.255"
 print " deny ip any 172.16.0.0 0.15.255.255"
 print " deny ip any 192.168.0.0 0.0.255.255"
 for subnet in pub:
-        print " permit ip",str(IPNetwork(subnet).network),getwild(subnet),"any" 
+        print " permit ip",str(IPNetwork(subnet).network),get_wildcard_mask(subnet),"any" 
 print " deny ip any any"
 print "!"
 
